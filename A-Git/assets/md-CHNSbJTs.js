6abaa2de694b5d807f6f9fd26e483c49269d0870
Chun <caochun@gmail.com>
2026-06-09 21:15:46 +0800

    Publish Git slides

---
+++ A-Git/assets/md-CHNSbJTs.js
@@ -0,0 +1,22 @@
+import{_ as r}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-BVBdn41c.js";import{o,b as u,w as a,g as s,d as c,m,ad as n,v as d,x as f,T as e}from"./modules/vue-U4KdHHdq.js";import{I as k}from"./slidev/default-BG8UxXe-.js";import{u as v,f as _}from"./slidev/context-D9uUBkke.js";import"./modules/unplugin-icons-DqFNU4b1.js";import"./index-BdgMKK0x.js";import"./modules/shiki-CmfFBYOm.js";const T={__name:"A-Git.md__slidev_41",setup(g){const{$clicksContext:p,$frontmatter:i}=v();return p.setup(),(h,l)=>{const t=r;return o(),u(k,d(f(e(_)(e(i),40))),{default:a(()=>[l[1]||(l[1]=s("h1",null,"从零构建路径回顾",-1)),l[2]||(l[2]=s("br",null,null,-1)),c(t,m({},{title:"",ranges:[]}),{default:a(()=>[...l[0]||(l[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null,"保存目录历史")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  |")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  v")]),n(`
+`),s("span",{class:"line"},[s("span",null,"保存文件内容：blob")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  |")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  v")]),n(`
+`),s("span",{class:"line"},[s("span",null,"保存目录结构：tree")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  |")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  v")]),n(`
+`),s("span",{class:"line"},[s("span",null,"保存版本关系：commit DAG")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  |")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  v")]),n(`
+`),s("span",{class:"line"},[s("span",null,"给历史起名字：refs + HEAD")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  |")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  v")]),n(`
+`),s("span",{class:"line"},[s("span",null,"组织下一次提交：index")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  |")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  v")]),n(`
+`),s("span",{class:"line"},[s("span",null,"压缩和传输：packfile")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  |")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  v")]),n(`
+`),s("span",{class:"line"},[s("span",null,"分布式协作：fetch / push")])])],-1)])]),_:1},16)]),_:1},16)}}};export{T as default};
---
+++ A-Git/assets/md-CVzaZHxg.js
@@ -0,0 +1,17 @@
+import{_ as d}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-BVBdn41c.js";import{o as u,b as f,w as a,g as s,d as i,m as t,ad as n,v as k,x as g,T as p}from"./modules/vue-U4KdHHdq.js";import{_ as m}from"./slidev/two-cols.vue_vue_type_script_setup_true_lang-a5qLg23D.js";import{u as c,f as h}from"./slidev/context-D9uUBkke.js";import"./modules/unplugin-icons-DqFNU4b1.js";import"./index-BdgMKK0x.js";import"./modules/shiki-CmfFBYOm.js";const N={__name:"A-Git.md__slidev_23",setup(v){const{$clicksContext:r,$frontmatter:o}=c();return r.setup(),(b,l)=>{const e=d;return u(),f(m,k(g(p(h)(p(o),22))),{right:a(x=>[l[3]||(l[3]=s("br",null,null,-1)),l[4]||(l[4]=s("p",null,"反方向也存在：",-1)),i(e,t({},{title:"",ranges:[]}),{default:a(()=>[...l[1]||(l[1]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null,"repository")]),n(`
+`),s("span",{class:"line"},[s("span",null,"     |")]),n(`
+`),s("span",{class:"line"},[s("span",null,"     | git checkout / restore")]),n(`
+`),s("span",{class:"line"},[s("span",null,"     v")]),n(`
+`),s("span",{class:"line"},[s("span",null,"index")]),n(`
+`),s("span",{class:"line"},[s("span",null,"     |")]),n(`
+`),s("span",{class:"line"},[s("span",null,"     v")]),n(`
+`),s("span",{class:"line"},[s("span",null,"working tree")])])],-1)])]),_:1},16),l[5]||(l[5]=s("p",null,"这解释了很多 Git 命令为什么看起来绕：",-1)),i(e,t({},{title:"",ranges:[]}),{default:a(()=>[...l[2]||(l[2]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null,"它们不是只改文件")]),n(`
+`),s("span",{class:"line"},[s("span",null,"而是在三个区域之间搬运快照")])])],-1)])]),_:1},16)]),default:a(()=>[l[6]||(l[6]=s("h1",null,"三个区域：命令怎么移动数据？",-1)),l[7]||(l[7]=s("br",null,null,-1)),i(e,t({},{title:"",ranges:[]}),{default:a(()=>[...l[0]||(l[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null,"working tree")]),n(`
+`),s("span",{class:"line"},[s("span",null,"     |")]),n(`
+`),s("span",{class:"line"},[s("span",null,"     | git add")]),n(`
+`),s("span",{class:"line"},[s("span",null,"     v")]),n(`
+`),s("span",{class:"line"},[s("span",null,"index")]),n(`
+`),s("span",{class:"line"},[s("span",null,"     |")]),n(`
+`),s("span",{class:"line"},[s("span",null,"     | git commit")]),n(`
+`),s("span",{class:"line"},[s("span",null,"     v")]),n(`
+`),s("span",{class:"line"},[s("span",null,"repository")])])],-1)])]),_:1},16)]),_:1},16)}}};export{N as default};
